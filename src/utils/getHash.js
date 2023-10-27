8fd79d2b83d9765e19ffb5cdc0e82280b4427eb8
josem3612 <jmu3612@gmail.com>
2023-10-27 05:16:46 -0500

    obtengo los personajes y los renderizo en la página

---
+++ src/utils/getHash.js
@@ -0,0 +1,6 @@
+const getHash = () => location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/' // ['', '1', '']
+
+export default getHash;
+
+
+//const getHash = () => location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/'; export default getHash;
